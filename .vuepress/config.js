f955194898b7c642f1a735a46cd392ba4aa660d5
amanej <aman.mender@gmail.com>
2019-11-13 23:16:32 +0100

    rename files

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,5 @@
+const locales = require('./locales.js');
+
 module.exports = {
   title: "JAMstack Oslo",
   description:
@@ -10,51 +12,6 @@ module.exports = {
     "@vuepress/back-to-top": {}
   },
   themeConfig: {
-    nav: [
-      { text: "Meetup", link: "https://www.meetup.com/JAMstack-Oslo/" },
-      { text: "Discord", link: "https://discord.gg/rE3pcSw" },
-      { text: "GitHub", link: "https://github.com/JAMstack-Oslo/org-website" }
-    ],
-    sidebar: [
-      "/",
-      {
-        title: "About",
-        collapsable: false,
-        children: [
-          "about/contribute.md",
-          "about/format.md",
-          "about/sponsor.md",
-          "about/contact.md"
-        ]
-      },
-      {
-        title: "Events",
-        collapsable: false,
-        children: [
-          "events/upcoming.md",
-          "events/speaking.md",
-          "/events/past.md",
-          "about/feedback.md"
-        ]
-      },
-      {
-        title: "Organizing",
-        collapsable: true,
-        children: ["/organization/checklist", "/organization/intro-outro"]
-      },
-      {
-        title: "Meetings",
-        collapsable: true,
-        children: [
-          "/meetings/notes.md",
-          "/meetings/meeting-planning-22-12-2018",
-          "/meetings/meeting-planning-03-01-2019",
-          "/meetings/meeting-planning-05-01-2019",
-          "/meetings/meeting-planning-29-01-2019",
-          "/meetings/meeting-planning-05-02-2019",
-          "/meetings/meeting-planning-26-02-2019"
-        ]
-      }
-    ]
-  }
+    locales: locales
+  },
 };
--- .vuepress/locales.js
+++ .vuepress/locales.js
@@ -14,20 +14,20 @@ module.exports = {
               title: "Om",
               collapsable: false,
               children: [
-                "about/contribute.md",
-                "about/format.md",
-                "about/sponsor.md",
-                "about/contact.md"
+                "om/contribute.md",
+                "om/format.md",
+                "om/sponsor.md",
+                "om/contact.md"
               ]
             },
             {
               title: "Arrangementer",
               collapsable: false,
               children: [
-                "events/upcoming.md",
-                "events/speaking.md",
-                "/events/past.md",
-                "about/feedback.md"
+                "arrangementer/kommende.md",
+                "arrangementer/talere.md",
+                "arrangementer/tidligere.md",
+                "om/tilbakemeldinger.md"
               ]
             },
             {
@@ -50,7 +50,7 @@ module.exports = {
             }
           ]
     },
-    '/en/': {
+    '/eng/': {
         lang: 'en-US',
         title: 'JAMstack Oslo',
         description: 'Notes on the organization and planning of JAMstack Oslo chapter',
@@ -65,38 +65,41 @@ module.exports = {
               title: "About",
               collapsable: false,
               children: [
-                "about/contribute.md",
-                "about/format.md",
-                "about/sponsor.md",
-                "about/contact.md"
+                "eng/about/contribute.md",
+                "eng/about/format.md",
+                "eng/about/sponsor.md",
+                "eng/about/contact.md"
               ]
             },
             {
               title: "Events",
               collapsable: false,
               children: [
-                "events/upcoming.md",
-                "events/speaking.md",
-                "/events/past.md",
-                "about/feedback.md"
+                "eng/events/upcoming.md",
+                "eng/events/speaking.md",
+                "eng/events/past.md",
+                "eng/about/feedback.md"
               ]
             },
             {
               title: "Organizing",
               collapsable: true,
-              children: ["/organization/checklist", "/organization/intro-outro"]
+              children: [
+                "eng/organization/checklist", 
+                "eng/organization/intro-outro"
+              ]
             },
             {
               title: "Meetings",
               collapsable: true,
               children: [
-                "/meetings/notes.md",
-                "/meetings/meeting-planning-22-12-2018",
-                "/meetings/meeting-planning-03-01-2019",
-                "/meetings/meeting-planning-05-01-2019",
-                "/meetings/meeting-planning-29-01-2019",
-                "/meetings/meeting-planning-05-02-2019",
-                "/meetings/meeting-planning-26-02-2019"
+                "eng/meetings/notes.md",
+                "eng/meetings/meeting-planning-22-12-2018",
+                "eng/meetings/meeting-planning-03-01-2019",
+                "eng/meetings/meeting-planning-05-01-2019",
+                "eng/meetings/meeting-planning-29-01-2019",
+                "eng/meetings/meeting-planning-05-02-2019",
+                "eng/meetings/meeting-planning-26-02-2019"
               ]
             }
           ]
